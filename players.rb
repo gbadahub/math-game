f474d5feaa4a9c8808ad39888331a5b6442b109d
Georgina Bada <georginabada@gmail.com>
2022-01-31 20:48:45 -0500

    implemented and tested players.rb

--- players.rb
+++ players.rb
@@ -1,15 +1,32 @@
+ require "./questions"
 class Players
-  attr_reader :name #getter
+  attr_reader :name, :lives
 
- def initialize (name)
+ def initialize(name)
   @name = name
   @lives = 3
  end 
 
- 
+ def lost_game
+  @lives == 0
+ end
+
+ def lost_round
+@lives -= 1
+ end
+
+def game_question
+  game_question = Questions.new
+   game_question.question("#{name}")   
+  @user_input = gets.chomp
+  if (game_question.answer(@user_input))
+  else 
+    puts 'Seriously? No!'
+    lost_round
+  end
+end
 
 end
 
-player1 = Players.new("Georgina")
-
-puts player1.name
+player1= Players.new("georgina")
+puts player1.game_question
--- questions.rb
+++ questions.rb
@@ -5,17 +5,18 @@ def initialize
   @value2 = rand (1...30)
 end
 
-def question (name)
+def question(name)
   puts "#{name}: What does #{@value1} plus #{@value2} equal to? "
 end
 
-def answer 
-  @sum = value1 + value2
-  puts "The correct answer is #{@sum}."
+def answer(user_input)
+  @sum = @value1 + @value2
+  
+  if ((user_input).to_i == @sum)
+    puts "The correct answer is #{@sum}."
+   return true 
+end 
 end 
 
 end
 
-new_question = Questions.new
-
-new_question.question("Georgina")
